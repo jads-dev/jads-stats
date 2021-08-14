8c0cd446745febc2b56c082bb4501a87b590f5fd
Nodja <me@nodja.com>
2021-08-14 04:13:16 -0400

    cleanup some stuff

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,13 +1,8 @@
 import colors from "vuetify/es5/util/colors";
 
 export default {
-  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
-
-  // Target: https://go.nuxtjs.dev/config-target
   target: "static",
-
-  // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: "%s - jads-stats",
     title: "jads-stats",
@@ -22,33 +17,17 @@ export default {
     ],
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
-
-  // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
-
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-
   plugins: [{ src: "@plugins/db.js" }],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    // https://go.nuxtjs.dev/vuetify
-    "@nuxtjs/vuetify"
-  ],
+  buildModules: ["@nuxtjs/vuetify"],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    // https://go.nuxtjs.dev/axios
-    "@nuxtjs/axios"
-  ],
+  modules: ["@nuxtjs/axios"],
 
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {},
 
-  // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
     customVariables: ["~/assets/variables.scss"],
     theme: {
@@ -67,16 +46,5 @@ export default {
     }
   },
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-    extend(config, ctx) {
-      config.module.rules.push({
-        test: /\.(ogg|mp3|wav|mpe?g)$/i,
-        loader: "file-loader",
-        options: {
-          name: "[path][name].[ext]"
-        }
-      });
-    }
-  }
+  build: {}
 };
